243f4b473c0bc9fb977614f1f7fa3ae4f4a813a8
jaehunshin <jhshin0422@gmail.com>
2026-05-10 12:38:42 +0900

    init: 코드 포매터 설정 추가

--- src/main/java/org/sopt/kakaotalk/KakaotalkApplication.java
+++ src/main/java/org/sopt/kakaotalk/KakaotalkApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KakaotalkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KakaotalkApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(KakaotalkApplication.class, args);
+  }
 }
