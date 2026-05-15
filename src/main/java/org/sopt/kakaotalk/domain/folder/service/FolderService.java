e3d323f859892cffaf16f44fdbc88576e98ba3ca
jaehunshin <jhshin0422@gmail.com>
2026-05-15 17:48:45 +0900

    feat: FolderService 구현

---
+++ src/main/java/org/sopt/kakaotalk/domain/folder/service/FolderService.java
@@ -0,0 +1,19 @@
+package org.sopt.kakaotalk.domain.folder.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.kakaotalk.domain.folder.dto.response.FolderListResponse;
+import org.sopt.kakaotalk.domain.folder.repository.FolderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FolderService {
+
+    private final FolderRepository folderRepository;
+
+    public FolderListResponse getFolders() {
+        return FolderListResponse.from(folderRepository.findAll());
+    }
+}
