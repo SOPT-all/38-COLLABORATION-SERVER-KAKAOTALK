8d0167397d87583d99d20e2d8e1636fff43adf72
jstar_00 <wltjd6300@naver.com>
2026-05-16 00:50:01 +0900

    fix: BaseResponse에 @Schema 추가
    - swagger 자동 생성 example(placeholder)에 swagger 자동 생성 placeholder 대신 정상적인 응답을 보여주도록 @Schema 적용

--- src/main/java/org/sopt/kakaotalk/global/response/BaseResponse.java
+++ src/main/java/org/sopt/kakaotalk/global/response/BaseResponse.java
@@ -1,9 +1,14 @@
 package org.sopt.kakaotalk.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.sopt.kakaotalk.global.code.ErrorCode;
 import org.sopt.kakaotalk.global.code.SuccessCode;
 
-public record BaseResponse<T>(int status, String code, String message, T data) {
+public record BaseResponse<T>(
+    @Schema(description = "HTTP 상태 코드", example = "200") int status,
+    @Schema(description = "서버 커스텀 응답 코드", example = "COM_200_001") String code,
+    @Schema(description = "응답 메시지", example = "요청이 성공적으로 처리되었습니다.") String message,
+    @Schema(description = "실제 응답 데이터") T data) {
 
   public static <T> BaseResponse<T> success(SuccessCode successCode, T data) {
     return new BaseResponse<>(
